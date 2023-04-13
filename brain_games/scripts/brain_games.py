dc9d1f286e88b578631606fa64242015e25c17d3
Dima <dima_rood@inbox.ru>
2023-04-13 20:03:12 +0300

    Pack libraries

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
+def welcome():
+    print("Welcome to the Brain Games!")
+
 
 def main():
-    print('Welcome to the Brain Games!')
- 
-if __name__ == '__main__':
+    welcome()
+    welcome_user()
+
+
+if __name__ == "__main__":
     main()
