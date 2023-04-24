4137de94ebbf6a94e37139592d11551d2ca2c093
Dima <dima_rood@inbox.ru>
2023-04-24 21:41:33 +0300

    Pack even games

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,13 @@
+from brain_games.game.even_game import welcome, welcome_user, quest_answer, game_process
+
+
+def main():
+    welcome()
+    name = welcome_user()
+    quest_answer()
+    game_process(name)
+
+
+
+if __name__ == '__main__':
+    main()
