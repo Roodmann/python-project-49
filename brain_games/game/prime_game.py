a914dcb43a62940f535061e0500accf2a4dd2b29
Dima <dima_rood@inbox.ru>
2023-06-06 22:51:24 +0300

    Full pack releas

--- brain_games/game/prime_game.py
+++ brain_games/game/prime_game.py
@@ -8,7 +8,7 @@ END_RANGE = 100
 
 def is_prime(number):
     """Выявляем True и Fals"""
-    for i in range(2, (number // 1) + 1):
+    for i in range(2, (number // 2) + 1):
         if number % i == 0:
             return False
     return True
